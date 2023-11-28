f71cf54ebad2d4029b03c625a32efd64c9acccdb
SaidLamghari <said.lmg@gmail.com>
2023-11-28 14:04:35 -0800

    100-print_tebahpla.py

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Start of  program that prints the ASCII alphabet,
+    in reverse order, alternating lowercase and uppercase 
+    (z in lowercase and Y in uppercase)"""
+
+
+
+for val in range(ord('z'), ord('a') - 1, -1):
+    print(chr(val) if val % 2 == 0 else chr(val - 32), end="")
