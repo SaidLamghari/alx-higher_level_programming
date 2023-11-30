0458617288697b2ba77d4bdb988df599fbc1dc58
SaidLamghari <said.lmg@gmail.com>
2023-11-30 07:01:32 -0800

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,4 +9,4 @@ if __name__ == '__main__':
     else:
         print("{} argument{}:".format(n_as, 's' if n_as != 1 else ''))
         for i in range(n_as):
-            print("{}: {}".format(i, arg))
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
