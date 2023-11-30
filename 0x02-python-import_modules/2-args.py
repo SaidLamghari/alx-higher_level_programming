c67efcb0031aea0ce647f83966e5fbcf140c8807
SaidLamghari <said.lmg@gmail.com>
2023-11-30 06:49:20 -0800

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
     if num_args == 0:
         print("0 argument.")
     else:
-        print("{} argument{}:".format(num_args, 's' if num_args != 1 else ''))
+        print("{} argument{}:".format(num_args, 's'
+            if num_args != 1 else ''))
         for i, arg in enumerate(args, start=1):
             print("{}: {}".format(i, arg))
