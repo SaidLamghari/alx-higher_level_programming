1d9e9f5345d5d5cecb568d30f879f572e8d968f0
SaidLamghari <said.lmg@gmail.com>
2023-11-30 08:36:45 -0800

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,9 @@ if __name__ == '__main__':
     n_as = len(sys.argv) - 1
     if n_as == 0:
         print("0 argument.")
+    elif n_as == 1:
+        print("1 argument:")
+        print("{}: {}".format(1, sys.argv[1]))
     else:
         print("{} argument{}:".format(n_as, 's' if n_as != 1 else ''))
         for i in range(n_as):
