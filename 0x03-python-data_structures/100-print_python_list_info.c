ea2d7df15683d634a437c0d2aae16bfb25c56a09
SaidLamghari <said.lmg@gmail.com>
2023-12-04 11:20:21 -0800

    100-print_python_list_info.c

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -10,7 +10,7 @@ void print_python_list_info(PyObject *p)
 	Py_ssize_t s, count;
 	PyObject *it;
 
-	size = PyList_Size(p);
+	s = PyList_Size(p);
 	
 	printf("[*] Size of the Python List = %ld\n", s);
 
