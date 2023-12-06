d8aac9b9d6b61a80d41643bfc26536a9494e4ebc
SaidLamghari <said.lmg@gmail.com>
2023-12-06 13:47:47 -0800

    103-python.c

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,74 @@
+#include <Python.h>
+
+/**
+ * print_python_list - Start of function
+ *		that prints basic information about Python lists
+ * @p: The pointer to a Python
+ */
+void print_python_list(PyObject *p)
+{
+	Py_ssize_t sz;
+	Py_ssize_t ac;
+	PyObject *itm;
+	Py_ssize_t count;
+
+	sz = PyList_Size(p);
+	ac = ((PyListObject *)p)->allocated;
+
+	printf("[*] Python list info\n");
+
+	printf("[*] Size of the Python List = %zd\n", sz);
+
+	printf("[*] Allocated = %zd\n", ac);
+
+	count = 0;
+	while (count < sz)
+	{
+		itm = PyList_GetItem(p, count);
+
+		printf("Element %zd: %s\n", count, Py_TYPE(itm)->tp_name);
+		count++;
+	}
+}
+
+/**
+ * print_python_bytes - Start of function that prints
+ *			basic information about Python bytes objects
+ * @p: The pointer to a Python
+ */
+void print_python_bytes(PyObject *p)
+{
+
+	Py_ssize_t sz;
+	char *str;
+	Py_ssize_t count;
+
+	printf("[.] bytes object info\n");
+
+	if (!PyBytes_Check(p))
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+
+		return;
+	}
+	sz = PyBytes_Size(p);
+
+	str = PyBytes_AsString(p);
+
+	printf("  size: %zd\n", sz);
+
+	printf("  trying string: %s\n", str);
+
+	printf("  first %zd bytes:", sz + 1 > 10 ? 10 : sz + 1);
+
+	count = 0;
+
+	while (count < sz + 1 && count < 10)
+	{
+		printf(" %02x", (unsigned char)str[count]);
+
+		count++;
+	}
+
+	printf("\n");
+}
