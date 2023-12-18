feabfe9cdd31136cee913bc7fba84e7176f9371b
SaidLamghari <said.lmg@gmail.com>
2023-12-18 06:25:16 -0800

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+#The function that prints x elements of a list.
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        for i in my_list:
+            print(i, end="")
+            count += 1
+            if count == x:
+                break
+    except TypeError:
+        pass
+    finally:
+        print()
+    return count
