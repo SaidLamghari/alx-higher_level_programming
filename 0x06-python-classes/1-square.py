cbe50255df7793e71d282e524fcb400486e8052c
SaidLamghari <said.lmg@gmail.com>
2023-12-19 10:47:59 -0800

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
+# class Square that defines a square by: (based on 0-square.py)
+
 class Square:
     def __init__(self, size):
         self.__size = size
 
-    @property
     def size(self):
         return self.__size
 
-    @size.setter
     def size(self, value):
         self.__size = value
