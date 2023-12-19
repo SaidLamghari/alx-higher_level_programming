b1409b0aae68aaf1e4ea1fb5dc861f9ad463e9f1
SaidLamghari <said.lmg@gmail.com>
2023-12-19 13:43:41 -0800

    100-singly_linked_list.py

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,57 +1,55 @@
 #!/usr/bin/python3
-"""class Node that defines a node of a singly linked"""
-
+"""class Node that defines a node of a singly linked list"""
 
 
 class Node:
     def __init__(self, data, next_node=None):
-        self.data = data
+        self._data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        return self.__data
+        return self._data
 
     @data.setter
     def data(self, value):
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
-        self.__data = value
+        self._data = value
 
     @property
     def next_node(self):
-        return self.__next_node
+        return self._next_node
 
     @next_node.setter
     def next_node(self, value):
         if value is not None and not isinstance(value, Node):
             raise TypeError("next_node must be a Node object")
-        self.__next_node = value
+        self._next_node = value
 
 
 class SinglyLinkedList:
     def __init__(self):
-        self.head = None
+        self._head = None
 
     def sorted_insert(self, value):
         new_node = Node(value)
-        if self.head is None:
-            self.head = new_node
-        elif value < self.head.data:
-            new_node.next_node = self.head
-            self.head = new_node
+        if self._head is None:
+            self._head = new_node
+        elif value < self._head.data:
+            new_node.next_node = self._head
+            self._head = new_node
         else:
-            current = self.head
-            while (current.next_node is not None and
-                   current.next_node.data < value):
+            current = self._head
+            while current.next_node is not None and current.next_node.data < value:
                 current = current.next_node
             new_node.next_node = current.next_node
             current.next_node = new_node
 
     def __str__(self):
-        result = ""
-        current = self.head
+        current = self._head
+        nodes = []
         while current is not None:
-            result += str(current.data) + "\n"
+            nodes.append(str(current.data))
             current = current.next_node
-        return result.strip()
+        return "\n".join(nodes)
