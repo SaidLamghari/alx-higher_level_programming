47b9295bc3969b679ff8130f3dddd3dcb84f6983
SaidLamghari <said.lmg@gmail.com>
2023-12-19 12:50:19 -0800

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""class Square that defines a square"""
+
+
+class Square:
+    """ Square """
+    def __init__(self, size=0):
+        """initialize data
+
+        Args:
+            size Square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
