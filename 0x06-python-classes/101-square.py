0f42ce80265ff9c9b540df4a3052a5ffefbd341d
SaidLamghari <said.lmg@gmail.com>
2023-12-19 14:16:33 -0800

    101-square.py

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""class Square that defines a square"""
 
 
 class Square:
+    """ Square """
     def __init__(self, size=0, position=(0, 0)):
         self.__size = 0
         self.__position = (0, 0)
