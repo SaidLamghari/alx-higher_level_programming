87059939d3f640f9c1d72d8c57e095b6c26bf462
SaidLamghari <said.lmg@gmail.com>
2024-01-07 15:58:52 -0800

    3-say_my_name.py

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""unction that prints My name is <first name> <last name>"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints "My name is <first_name> <last_name>".
+
+    Args:
+        first_name (str): The first name.
+        last_name (str): The last name (default is an empty string).
+
+    Raises:
+        TypeError: If either first_name or last_name is not a string.
+
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testfile("tests/3-say_my_name.txt")
