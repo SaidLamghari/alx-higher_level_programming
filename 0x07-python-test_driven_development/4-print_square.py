11ccd63310715216d344564dc3f8a98a86fef13c
SaidLamghari <said.lmg@gmail.com>
2024-01-07 16:29:37 -0800

    4-print_square.py

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""function that prints a square with the character #."""
+
+
+def print_square(size):
+    """
+    Prints a square with the character '#'.
+
+    Args:
+        size (int): The size length of the square.
+
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is less than 0.
+        TypeError: If size is a float and is less than 0.
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+        print("#" * size)
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testfile("tests/4-print_square.txt")
