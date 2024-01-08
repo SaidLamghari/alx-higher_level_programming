39a88aebbbf0e307c0853b7c222b09bd2c0013ba
SaidLamghari <said.lmg@gmail.com>
2024-01-08 12:13:28 -0800

    10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""class Square that inherits from Rectangle
+(9-rectangle.py):"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    A class representing a square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a Square instance with the given size.
+
+        Args:
+            size (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than or equal to 0.
+
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def __str__(self):
+        """
+        Returns a string representation of the Square.
+
+        Returns:
+            str: A string representation of the Square.
+
+        """
+        return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__height)
