4f36f8fb528dc28c7d685a304a9367d503d64207
SaidLamghari <said.lmg@gmail.com>
2024-01-14 14:56:05 -0800

    models/rectangle.py

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+""" models/rectangle.py """
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ Rectangle class that inherits from Base """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initializes a Rectangle instance.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+            x (int): The x-coordinate of the rectangle's position.
+            y (int): The y-coordinate of the rectangle's position.
+            id (int): The id of the rectangle. If not provided, it will be handled
+                by the Base class.
+
+        Attributes:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+            x (int): The x-coordinate of the rectangle's position.
+            y (int): The y-coordinate of the rectangle's position.
+            id (int): The id of the rectangle.
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """ Getter for the width attribute """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Setter for the width attribute """
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Getter for the height attribute """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Setter for the height attribute """
+        self.__height = value
+
+    @property
+    def x(self):
+        """ Getter for the x attribute """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ Setter for the x attribute """
+        self.__x = value
+
+    @property
+    def y(self):
+        """ Getter for the y attribute """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """ Setter for the y attribute """
+        self.__y = value
