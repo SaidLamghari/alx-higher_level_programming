0c324857e1b375800595db35a05f6290c0c78b1c
SaidLamghari <said.lmg@gmail.com>
2024-02-13 09:15:15 -0800

    2-remove_database.sql

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- Usage: cat 2-remove_database.sql | mysql -hlocalhost -uroot -p
+-- Author: SAID LAMGHARI
+-- Delete the hbtn_0c_0 database if it exists
+-- Date: February 13, 2024
+DROP DATABASE IF EXISTS hbtn_0c_0;
