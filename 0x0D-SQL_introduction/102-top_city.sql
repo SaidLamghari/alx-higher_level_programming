259e669037f8fff692cbfd4329a486f1d055f722
SaidLamghari <said.lmg@gmail.com>
2024-02-13 13:55:24 -0800

    102-top_city.sql

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,5 @@
+-- Script: 102-top_city.sql
+-- Description: A script to display the top 3 cities' temperatures during July and August, ordered by temperature in descending order.
+-- SAID LAMGHARI
+-- Display the top 3 cities' temperatures during July and August
+SELECT city, AVG(value) AS average_tmp FROM temperatures WHERE month IN (7, 8) GROUP BY city ORDER BY average_tmp DESC LIMIT 3;
