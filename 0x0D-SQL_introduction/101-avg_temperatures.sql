ca10d9ac6043e7aa9730a421397cc2eb56793065
SaidLamghari <said.lmg@gmail.com>
2024-02-13 13:30:20 -0800

    101-avg_temperatures.sql

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,8 @@
+-- Script: 101-avg_temperatures.sql
+-- Description: A script to display the average temperature (in Fahrenheit) by city, ordered by temperature in descending order.
+-- Author: SAID LAMGHARI
+-- Display the average temperature by city
+SELECT city, AVG(temperature) AS avg_temp
+FROM weather
+GROUP BY city
+ORDER BY avg_temp DESC;
