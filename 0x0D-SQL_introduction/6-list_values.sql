20d05dba91ad46d46d5089aa599b1cc74f2b2b9b
SaidLamghari <said.lmg@gmail.com>
2024-02-13 11:29:40 -0800

    6-list_values.sql

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,7 +1,5 @@
 -- Script: 6-list_values.sql
 -- Description: A script to list all rows of the table 'first_table' from the database 'hbtn_0c_0' in MySQL server.
 -- Auteur: SAID LAMGHARI
--- Step 1: Select the database
-USE hbtn_0c_0;
--- Step 2: Retrieve all rows from the 'first_table'
+-- Retrieve all rows from the 'first_table'
 SELECT * FROM first_table;
