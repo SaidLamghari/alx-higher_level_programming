f59d30f927aecfaa7c63f0c06c963adabee49ac6
SaidLamghari <said.lmg@gmail.com>
2024-02-13 09:11:08 -0800

    1-create_database_if_missing.sql

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,5 @@
+-- Usage: cat 1-create_database_if_missing.sql | mysql -hlocalhost -uroot -p
+-- Author: SAID LAMGHARI
+-- Create the hbtn_0c_0 database if it doesn't exist
+-- Date: February 13, 2024
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
