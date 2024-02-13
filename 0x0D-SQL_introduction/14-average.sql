7cce58147d0d7114c8e5b03124aa223365136a7b
SaidLamghari <said.lmg@gmail.com>
2024-02-13 12:32:26 -0800

    14-average.sql

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,5 @@
+-- Script: 14-average.sql
+-- Description: A script to compute the average score of all records in the 'second_table' of the 'hbtn_0c_0' database.
+-- Author: SAID LAMGHARI
+-- Compute the average score
+SELECT AVG(score) AS average FROM second_table;
