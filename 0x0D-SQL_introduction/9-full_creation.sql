4ad4a60ff1a3fb85f14bdb1a19351b825f64d0f1
SaidLamghari <said.lmg@gmail.com>
2024-02-13 12:07:01 -0800

    9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,6 +2,7 @@
 -- Description: A script to create the table 'second_table' in the 'hbtn_0c_0' database and insert multiple rows.
 -- Author: SAID LAMGHARI
 -- Insert multiple rows into 'second_table'
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score) VALUES
     (1, 'John', 10),
     (2, 'Alex', 3),
