f4ad8a80a0c93a87db8d57d0e0dbde6b573ebb86
SaidLamghari <said.lmg@gmail.com>
2024-02-13 11:35:13 -0800

    7-insert_value.sql

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,5 @@
+-- Script: 7-insert_value.sql
+-- Description: A script to insert a new row into the table 'first_table' in the 'hbtn_0c_0' database.
+-- Author: SAID LAMGHARI
+-- Insert a new row into 'first_table'
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
