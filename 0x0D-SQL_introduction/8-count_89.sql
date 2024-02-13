8bc12f9b5811facb805e9588e6f970a3a1baede8
SaidLamghari <said.lmg@gmail.com>
2024-02-13 11:38:16 -0800

    8-count_89.sql

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,5 @@
+-- Script: 8-count_89.sql
+-- Description: A script to count the number of records with id = 89 in the table 'first_table' in the 'hbtn_0c_0' database.
+-- Author: SAID LAMGHARI
+-- Count the number of records with id = 89
+SELECT COUNT(*) FROM first_table WHERE id = 89;
