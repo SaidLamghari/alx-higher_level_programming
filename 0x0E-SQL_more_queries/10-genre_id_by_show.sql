dd6d171f1830a8b89b3138025423efc545076738
SaidLamghari <said.lmg@gmail.com>
2024-02-14 09:34:21 -0800

    10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -8,5 +8,5 @@
 -- The results are sorted in ascending order by tv_shows.title and tv_show_genres.genre_id.
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
