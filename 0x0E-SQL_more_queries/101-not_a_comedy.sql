bb4bfbc23dc527c90ec9e7f7e3c2a1ee21f47974
SaidLamghari <said.lmg@gmail.com>
2024-02-14 11:04:37 -0800

    101-not_a_comedy.sql

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -6,12 +6,8 @@
 SELECT tv_shows.title
 FROM tv_shows
 WHERE tv_shows.id NOT IN (
-    SELECT show_id
+    SELECT tv_show_genres.show_id
     FROM tv_show_genres
-    WHERE genre_id = (
-        SELECT id
-        FROM tv_genres
-        WHERE name = 'Comedy'
-    )
-)
+    JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_genres.name = 'Comedy')
 ORDER BY tv_shows.title ASC;
