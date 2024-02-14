c44473f4891016449d21d648eade6ea5e154cc84
SaidLamghari <said.lmg@gmail.com>
2024-02-14 09:27:32 -0800

    10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -6,7 +6,7 @@
 -- It uses a single SELECT statement to join the tv_shows and tv_show_genres tables,
 -- and retrieves the tv_shows.title and tv_show_genres.genre_id columns.
 -- The results are sorted in ascending order by tv_shows.title and tv_show_genres.genre_id.
-
+USE `hbtn_0d_tvshows`;
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
