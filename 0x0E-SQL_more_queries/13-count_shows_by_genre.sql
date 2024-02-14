8310863916d68d75e7cb65e6dcc7d048bbb7b793
SaidLamghari <said.lmg@gmail.com>
2024-02-14 10:28:35 -0800

    13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -7,8 +7,9 @@
 -- based on the tv_shows.id and tv_show_genres.show_id columns.
 -- It retrieves the genre names from the tv_show_genres table and counts the number of shows for each genre.
 -- The results are grouped by genre and sorted in descending order by the number of shows linked.
-SELECT tv_show_genres.genre_id AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
-FROM tv_show_genres
-GROUP BY tv_show_genres.genre_id
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.id
 HAVING number_of_shows > 0
 ORDER BY number_of_shows DESC;
