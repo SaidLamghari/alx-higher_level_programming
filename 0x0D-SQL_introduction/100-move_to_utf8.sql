a7a54bc45890ddaf65dab1032733abcde9bfc020
SaidLamghari <said.lmg@gmail.com>
2024-02-14 12:15:01 -0800

    100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,9 +1,6 @@
 -- Script: 100-move_to_utf8.sql
 -- Author: SAID LAMGHARI
 -- Step 3: Convert the field to UTF8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-
-USE hbtn_0c_0;
 
 -- Create a new table with the desired collation
 CREATE TABLE new_table LIKE first_table;
