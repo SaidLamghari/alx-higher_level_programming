ba43235ddccdebc762cd0a2bb3103ad6164e2121
SaidLamghari <said.lmg@gmail.com>
2024-02-15 05:52:20 -0800

    100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -5,7 +5,6 @@
 -- Create database
 DROP DATABASE IF EXISTS hbtn_0c_0;
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE hbtn_0c_0;
 
 -- Create table
 CREATE TABLE IF NOT EXISTS first_table (
