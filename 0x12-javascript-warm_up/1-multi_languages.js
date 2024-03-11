8f8429ef63e6141f84635ea793c36b5a59ed0ff3
SaidLamghari <said.lmg@gmail.com>
2024-03-11 07:01:59 -0700

    1-multi_languages.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#/usr/bin/node
+// Autor: Said LAMGHARI
+
+const myLine1 = 'C is fun';
+const myLine2 = 'Python is cool';
+const myLine3 = 'JavaScript is amazing';
+
+console.log(myLine1);
+console.log(myLine2);
+console.log(myLine3);
+
