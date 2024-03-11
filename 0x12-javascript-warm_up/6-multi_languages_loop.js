8e8f524c25b184e15b77982eb3394f359211e1cd
SaidLamghari <said.lmg@gmail.com>
2024-03-11 13:08:36 -0700

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Autor: Said LAMGHARI
+
+let i;
+const ArrLines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (i = 0; i < ArrLines.length; i++) {
+  console.log(ArrLines[i]);
+}
