662c60a52ff4fa4d500f52eaf7e937e3a59ba7b8
SaidLamghari <said.lmg@gmail.com>
2024-03-11 08:04:42 -0700

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // Autor: Said LAMGHARI
 
-const NbreArgs = process.argv.length;
+const NbreArgs = process.argv;
 
-if (NbreArgs === 2) {
+if (NbreArgs.length === 2) {
   console.log('No argument');
-} else if (NbreArgs === 3) {
+} else if (NbreArgs.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
