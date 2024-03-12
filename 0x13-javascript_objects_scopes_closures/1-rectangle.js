836b9829d51d361a4f69abc2150c31e455d76ba7
SaidLamghari <said.lmg@gmail.com>
2024-03-12 09:46:05 -0700

    1-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// Autor: Said LAMGHARI
+// Class Rectangle that defines a rectangle
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
