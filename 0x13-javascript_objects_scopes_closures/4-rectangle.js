f93ef79399d028bff0108b446e9ca678244b2745
SaidLamghari <said.lmg@gmail.com>
2024-03-12 11:23:34 -0700

    4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+// Autor: Said LAMGHARI
+// class Rectangle that defines a rectangle
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let cnt = 0; cnt < this.height; cnt++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+}
+
+module.exports = Rectangle;
