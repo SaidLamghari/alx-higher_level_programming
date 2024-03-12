6a6cbe5950a12e58992a2ccfd9c161486ef2ffe3
SaidLamghari <said.lmg@gmail.com>
2024-03-12 14:18:16 -0700

    7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,12 +3,16 @@
 // Autor: Said LAMGHARI
 // a function that returns the number
 // of occurrences in a list
+
 exports.nbOccurences = function (list, searchElement) {
-  let CntOccu = 0;
+  let cntoc = 0;
+
     for (let cnt = 0; cnt < list.length; cnt++) {
       if (list[i] === searchElement) {
-        CntOccu++;
+        cntoc++;
       }
     }
-  return CntOccu;
+
+  return cntoc;
+
 };
