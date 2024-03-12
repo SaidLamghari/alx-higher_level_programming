a65549a4b9c24d9d1b7d46681de62417c7554e37
SaidLamghari <said.lmg@gmail.com>
2024-03-12 10:51:19 -0700

    3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -3,21 +3,24 @@
 // Autor: Said LAMGHARI
 // class Rectangle that defines a rectangle
 
-let cnt;
+let cnt1;
+let cnt2;
 
 class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
-    } else {
-      Object.create(null);
     }
   }
 
   print () {
-    for (cnt = 0; cnt < this.height; cnt++) {
-      console.log('X'.repeat(this.width));
+    for (cnt1 = 0; cnt1 < this.height; cnt1++) {
+      let wh = '';
+      for (cnt2 = 0; cnt2 < this.width; cnt2++) {
+        wh += 'X';
+      }
+      console.log(wh);
     }
   }
 }
