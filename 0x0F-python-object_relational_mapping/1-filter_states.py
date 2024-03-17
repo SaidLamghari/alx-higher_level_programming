2bd5d43afd8602774c19dae98f07a637ca4f060f
SaidLamghari <said.lmg@gmail.com>
2024-03-17 03:18:49 -0700

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
 
     # Connect to the server of MySQL
     db = MySQLdb.connect(
-        charset="utf8"
+        charset="utf8",
         host=host,
         port=port, user=UsName,
         passwd=PsWord,
