70c73ea0936064fc05646774c26295f62c8e71fa
SaidLamghari <said.lmg@gmail.com>
2024-03-17 04:43:20 -0700

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Displays the values
+in the states table
+of hbtn_0e_0_usa
+where name matches the argument
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    # MySQL setting
+    UsName = sys.argv[1]
+    PsWord = sys.argv[2]
+    DaBase = sys.argv[3]
+    SatName = sys.argv[4]
+    host = "localhost"
+    port = 3306
+
+    # Connect to the server of MySQL
+    db = MySQLdb.connect(
+        host=host, port=port,
+        user=UsName,
+        passwd=PsWord,
+        db=DaBase
+    )
+
+    # Create a ObjCursor to execute SQL
+    ObjCursor = db.cursor()
+
+    # The SQL to select states with the name that is provided
+    SqlQry = "SELECT * FROM states\
+        WHERE name = %s ORDER BY id ASC"
+    ObjCursor.execute(SqlQry, (SatName,))
+
+    # Return the QuRows
+    QuRows = ObjCursor.fetchall()
+
+    # Print
+    for PrRow in QuRows:
+        print(PrRow)
+
+    # Close the connection of DaBase and the ObjCursor
+    ObjCursor.close()
+
+    db.close()
