c9186f0d80b1d67b677588d51cd4d614db872e14
SaidLamghari <said.lmg@gmail.com>
2024-03-17 03:09:52 -0700

    1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Lists all states
+with a name starting with N (upper N)
+from the DaBase hbtn_0e_0_usa
+"""
+
+import sys
+
+import MySQLdb
+
+if __name__ == "__main__":
+    # MySQL setting
+    UsName = sys.argv[1]
+    PsWord = sys.argv[2]
+    DaBase = sys.argv[3]
+    host = "localhost"
+    port = 3306
+
+    # Connect to the server of MySQL
+    db = MySQLdb.connect(
+        host=host,
+        port=port, user=UsName,
+        passwd=PsWord,
+        db=DaBase
+    )
+
+    # Create a ObjCursor to execute SQL
+    ObjCursor = db.cursor()
+
+    # The SQL to select states that start with 'N'
+    ObjCursor.execute("SELECT * FROM states\
+            WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    # return the Rows
+    QuRows = ObjCursor.fetchall()
+
+    # Print
+    for PrRow in QuRows:
+        print(PrRow)
+
+    # Close the conection of DaBase and the ObjCursorv
+    ObjCursor.close()
+    db.close()
