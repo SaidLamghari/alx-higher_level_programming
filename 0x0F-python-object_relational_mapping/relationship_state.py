6f82fc6de5d7adf16bf159725c0dc86c105fc146
SaidLamghari <said.lmg@gmail.com>
2024-03-21 16:39:36 -0700

    relationship 101

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -29,7 +29,5 @@ class State(Base):
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-
     name = Column(String(128), nullable=False)
-
     cities = relationship("City", cascade="all, delete", backref="state")
