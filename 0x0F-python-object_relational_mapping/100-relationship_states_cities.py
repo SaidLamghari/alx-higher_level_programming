b82fe42fee8e9b9c04f67ae36015e11a156f9ead
SaidLamghari <said.lmg@gmail.com>
2024-03-21 15:49:21 -0700

    100-relationship_states_cities.py

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,41 +1,30 @@
 #!/usr/bin/python3
-'''
-SAID LAMGHARI
-'''
-from relationship_state import Base, State
-
+"""Lists all ObjState
+from the PsDaBase hbtn_0e_6_usa"""
 import sys
 
-from relationship_city import City
 from sqlalchemy import create_engine
+
 from sqlalchemy.orm import sessionmaker
+
+from model_state import Base, State
 
 
 if __name__ == "__main__":
-
+    # MySQL setting
     UsName = sys.argv[1]
-    PsWord = sys.argv[2]
-    PsDaBase = sys.argv[3]
-    host = "localhost"
     port = 3306
 
-    CrEng = create_engine(f"mysql+mysqldb://{UsName}:\
+    # Create the engine
+    # bind it to the session
+    engine = create_engine(f"mysql+mysqldb://{UsName}:\
             {PsWord}@{host}:{port}/{PsDaBase}")
-
-    Base.metadata.create_all(CrEng)
-
-    Session = sessionmaker(bind=CrEng)
-
+    Session = sessionmaker(bind=engine)
     session = Session()
 
-    NwStat = State(name="California")
+    # Retrieve objects
+    # sort them by ID
+    ObjStates = session.query(State).order_by(State.id).all()
 
-    NwCity = City(name="San Francisco", state=NwStat)
-
-    session.add(NwStat)
-
-    session.add(NwCity)
-
-    session.commit()
 
     session.close()
