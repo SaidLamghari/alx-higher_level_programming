3a4fec21126085c0148dab292932a751aa3f6793
SaidLamghari <said.lmg@gmail.com>
2024-03-27 14:39:47 -0700

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # Autor : Said LAMGHARI
-curl -sL -w "%{http_code}" -o /dev/null "$1" | {
-    read -r status_code
-    if [ "$status_code" -eq 200 ]; then
-        curl -sL "$1"
-    fi
-}
+curl -sL -w "%{http_code}" -o /dev/null "$1" | { read -r c; [ $c -eq 200 ] && curl -sL "$1" ; }
