8b4806adc5e3f1e6ae56500300b53ba2855d35fe
SaidLamghari <said.lmg@gmail.com>
2024-03-27 14:26:52 -0700

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script takes in a URL
 # Autor : Said LAMGHARI
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}'
