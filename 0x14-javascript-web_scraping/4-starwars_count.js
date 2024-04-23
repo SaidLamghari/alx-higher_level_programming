781af93045c307a87e7b8c0d9759eaec33a069c4
SaidLamghari <said.lmg@gmail.com>
2024-04-23 15:06:15 -0700

    4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,41 @@
+#!/usr/bin/node
+// Module requis pour effectuer des requêtes HTTP
+// Autor: Said LAMGHARI
+const request = require('request');
+
+// Récupération de l'URL de l'API Star Wars à
+// partir des arguments de la ligne de commande
+const apiLnk = process.argv[2];
+
+// ID du personnage "Wedge Antilles"
+const chrctrId = '18';
+
+// Envoi de la requête GET à l'URL spécifiée
+request.get(apiLnk, (erreur, response, bd) => {
+  // Si une erreur s'est produite lors
+  // de la requête, afficher l'objet d'erreur
+  if (erreur) {
+    console.error(erreur);
+    return;
+  }
+
+  // Analyse du corps de la réponse JSON
+  const flmsData = JSON.parse(bd).results;
+
+  // Initialisation du compteur de films
+  // où le personnage "Wedge Antilles" est présent
+  let cnt = 0;
+
+  // Parcourir chaque film pour vérifier la présence du personnage
+  flmsData.forEach((film) => {
+    // Vérifier si le personnage "Wedge Antilles"
+    // est présent dans la liste des personnages du film
+    if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${chrctrId}/`)) {
+      cnt++;
+    }
+  });
+
+  // Affichage du nombre de films où
+  // le personnage "Wedge Antilles" est présent
+  console.log(cnt);
+});
