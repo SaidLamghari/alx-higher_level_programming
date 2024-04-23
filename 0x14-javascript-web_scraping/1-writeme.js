d0057ade033a8feba04472cff90ff6363df77ef8
SaidLamghari <said.lmg@gmail.com>
2024-04-23 10:03:51 -0700

    1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+// Module requis pour les opérations sur le système de fichiers
+// Autor : Said LAMGHARI
+
+const fs = require('fs');
+
+// Extraction du chemin du fichier et du contenu à écrire
+// à partir des arguments de la ligne de commande
+const cheminFcher = process.argv[2];
+const contenu = process.argv[3];
+
+// Écriture du contenu dans le fichier spécifié
+fs.writeFile(cheminFcher, contenu, 'utf-8', (erreur) => {
+	// Si une erreur s'est produite lors de
+	// l'écriture, afficher l'objet d'erreur
+	if (erreur) {
+		console.error(erreur);
+		return;
+	}
+	// Afficher un message indiquant que le contenu
+	// a été écrit avec succès dans le fichier
+	if (!erreur) {
+		console.log(`Le contenu a été écrit dans ${cheminFcher}`);
+	}
+});
