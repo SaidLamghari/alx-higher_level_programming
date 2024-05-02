2eb715b81cc47e72885ee7d8d68528133b98f7b7
SaidLamghari <said.lmg@gmail.com>
2024-05-02 22:56:20 +0100

    8-script.js

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,8 @@
+// Autor: SAID LAMGHARI
+$(document).ready(function(){
+    $.get('https://swapi-api.alx-tools.com/api/films/?format=json', function(data){
+        data.results.forEach(function(movie){
+            $('UL#list_movies').append('<li>' + movie.title + '</li>');
+        });
+    });
+})
